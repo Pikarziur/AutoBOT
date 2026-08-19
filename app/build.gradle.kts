db6f8ac7025b752fac0a550867424f73e0954dfc
Pikarziur <102363012+Pikarziur@users.noreply.github.com>
2026-08-19 17:22:37 +0800

    v0.02

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.autobot.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.01"
+        versionCode = 2
+        versionName = "0.02"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
